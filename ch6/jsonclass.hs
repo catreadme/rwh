064a5af3b21617edc36614b97abe61b9955ab701
Robin Elvedi <trivialic@posteo.net>
2016-06-05 13:38:24 +0200

    jsonclass

--- ch6/jsonclass.hs
+++ ch6/jsonclass.hs
@@ -1,12 +1,18 @@
 newtype JsonString = JsonString [Char]
   deriving (Eq, Ord, Show, Read)
 
+newtype JsonObject a = JsonObject [(JsonString, a)]
+  deriving (Eq, Ord, Show, Read)
+
+newtype JsonArray a = JsonArray [a]
+  deriving (Eq, Ord, Show, Read)
+
 data JVal = JStr JsonString
           | JNum Double
           | JBool Bool
           | JNull
-          | JObj [(JsonString, JVal)]
-          | JArr [JVal]
+          | JObj (JsonObject JVal)
+          | JArr (JsonArray JVal)
           deriving (Eq, Ord, Show, Read)
 
 class Json a where
@@ -14,19 +20,38 @@ class Json a where
   fromJVal :: JVal -> Either String a
 
 instance Json JsonString where
-  toJVal x = JStr x
+  toJVal = JStr
 
   fromJVal (JStr x) = Right x
   fromJVal _ = Left "Not a String"
 
 instance Json Double where
-  toJVal x = JNum x
+  toJVal = JNum
 
   fromJVal (JNum x) = Right x
   fromJVal _ = Left "Not a Double"
 
 instance Json Bool where
-  toJVal x = JBool x
+  toJVal = JBool
 
   fromJVal (JBool x) = Right x
   fromJVal _ = Left "Not a Bool"
+
+instance (Json a) => Json (JsonArray a) where
+  toJVal = undefined
+  fromJVal = undefined
+
+instance (Json a) => Json (JsonObject a) where
+  toJVal = undefined
+  fromJVal = undefined
+
+
+-- helpers
+listToJVals :: (Json a) => [a] -> [JVal]
+listToJVals = map toJVal
+
+jValsToJArr :: [JVal] -> JsonArray JVal
+jValsToJArr = JsonArray
+
+jArrOfJValsToJVal :: JsonArray JVal -> JVal
+jArrOfJValsToJVal = JArr
